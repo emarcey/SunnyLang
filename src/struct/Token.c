49091f29732257597949bcf4ad23bb3e024c06d3
evanmarcey <evanmarcey@gmail.com>
2018-05-04 15:11:03 -0400

    Added descriptions for Token functions

--- src/struct/Token.c
+++ src/struct/Token.c
@@ -20,6 +20,7 @@ struct Token {
 	int token_precedence;
 };
 
+//Function to create a token
 struct Token * create_token(char token_type,
 		char * token_value) {
 	struct Token* token = (struct Token*) malloc(sizeof(struct Token));
@@ -31,12 +32,14 @@ struct Token * create_token(char token_type,
 	return token;
 }
 
+//Function to set a token's value; updates hash and precedence
 void assign_token_value(struct Token * token, char * new_value) {
 	token->token_value = new_value;
 	token->token_hash=hash(token->token_value);
 	token->token_precedence=get_precedence(token->token_hash);
 }
 
+//Function to append a string to the end of a token's value; updates hash and precedence
 void append_token_value(struct Token * token, char * new_value) {
 	char * tmp = malloc(sizeof(token->token_value)+sizeof(char)*(strlen(new_value)+2));
 	sprintf(tmp,"%s %s",token->token_value,new_value);
@@ -45,22 +48,27 @@ void append_token_value(struct Token * token, char * new_value) {
 	token->token_precedence=get_precedence(token->token_hash);
 }
 
+//Function to assign a type to a token
 void assign_token_type(struct Token * token, char new_type) {
 	token->token_type=new_type;
 }
 
+//Function returns a token's type
 char get_token_type(struct Token * token) {
 	return token->token_type;
 }
 
+//Function returns a token's value
 char * get_token_value(struct Token * token) {
 	return token->token_value;
 }
 
+//Function returns a token's hash value
 unsigned int get_token_hash(struct Token * token) {
 	return token->token_hash;
 }
 
+//Function returns a token's precedence
 int get_token_precedence(struct Token * token) {
 	return token->token_precedence;
 }
--- src/struct/Token.h
+++ src/struct/Token.h
@@ -8,22 +8,31 @@
 #ifndef STRUCT_TOKEN_H_
 #define STRUCT_TOKEN_H_
 
+//implements structure to store data for a Token
 struct Token;
 
+//Function to create a token
 struct Token * create_token(char token_type,char * token_value);
 
+//Function to set a token's value; updates hash and precedence
 void assign_token_value(struct Token * token, char * new_value);
 
+//Function to append a string to the end of a token's value; updates hash and precedence
 void append_token_value(struct Token * token, char * new_value);
 
+//Function to assign a type to a token
 void assign_token_type(struct Token * token, char new_type);
 
+//Function returns a token's type
 char get_token_type(struct Token * token);
 
+//Function returns a token's value
 char * get_token_value(struct Token * token);
 
+//Function returns a token's hash value
 unsigned int get_token_hash(struct Token * token);
 
+//Function returns a token's precedence
 int get_token_precedence(struct Token * token);
 
 #endif /* STRUCT_TOKEN_H_ */
